e755e1bd7688ee8a736809f34756993431f2b628
KaungZayY <kaungzayyan19@gmail.com>
2024-08-19 10:24:54 +0700

    Roles new field

--- database/migrations/2024_06_20_050721_create_roles_table.php
+++ database/migrations/2024_06_20_050721_create_roles_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->integer('team_id');
             $table->string('role_name');
+            $table->string('description');
             $table->timestamps();
             $table->softDeletes();
         });
