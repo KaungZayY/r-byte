d7a5b8e07783949a36fb9473c13885794bf781eb
KaungZayY <kaungzayyan19@gmail.com>
2024-09-07 22:40:38 +0700

    Permissions Stored in Cache to avoid N+1 Queries

--- app/Helpers/PermissionHelper.php
+++ app/Helpers/PermissionHelper.php
@@ -6,35 +6,47 @@ use App\Models\Project;
 use App\Models\Role;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cache;
 
 class PermissionHelper{
     
     public function isProjectOwner(Project $project): bool
     {
         $user = Auth::user();
-        return $user->id === $project->created_by;
+        $cacheKey = "is_user_{$user->id}_owner_of_{$project->id}";
+        return Cache::remember($cacheKey,now()->addMinutes(15), function () use ($user, $project){
+            return $user->id === $project->created_by;
+        });
     }
 
     public function getPermissionId($featureName,$permissionName): int
     {
-        $feature = Feature::where('feature_name',$featureName)->first();
-        if($feature){
-            $permission = $feature->permissions->where('permission_name',$permissionName)->first();
-            if($permission){
-                return $permission->id;
+        $cacheKey = "permission_id_of_{$featureName}_{$permissionName}_is";
+        return Cache::rememberForever($cacheKey, function() use ($featureName, $permissionName){
+            $feature = Feature::where('feature_name',$featureName)->first();
+            if($feature){
+                $permission = $feature->permissions->where('permission_name',$permissionName)->first();
+                if($permission){
+                    return $permission->id;
+                }
             }
-        }
-        return 0;
+            return 0;
+        });
     }
 
     public function checkPermission(Project $project, $permissionId): bool
     {
         $user = Auth::user();
-        $role_id = $user->roleForProject($project)->id;
-        if($role_id)
-        {
-            $role = Role::where('id',$role_id)->first();
-            return $role->permissions->contains('id', $permissionId);
+        $role_id = Cache::remember("user_{$user->id}_project_{$project->id}_role_is", now()->addMinutes(60), function () use ($user, $project) {
+            return $user->roleForProject($project)->id;
+        });
+        if ($role_id) {
+                $permissions = Cache::remember("role_{$role_id}_permissions", now()->addMinutes(60), function () use ($role_id) {
+                $role = Role::with('permissions')->find($role_id);
+                return $role ? $role->permissions->pluck('id')->toArray() : [];
+            });
+    
+            return in_array($permissionId, $permissions);
         }
         return false;
     }
