d6ca16aa6e49068b6df72674c341f5eab434cf0b
KaungZayY <kaungzayyan19@gmail.com>
2024-09-20 19:12:48 +0700

    Project Detail Current Sprint Name:

--- resources/views/projects/details-project.blade.php
+++ resources/views/projects/details-project.blade.php
@@ -6,7 +6,10 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="dark:bg-black overflow-hidden">  
+            <div class="dark:bg-black overflow-hidden">
+                <div class="text-4xl font-extrabold text-gray-900 dark:text-gray-100 items-center text-center mb-2">
+                    <span>{{ $sprint->sprint_name }}</span>
+                </div>
                 <livewire:ticket-board :sprint="$sprint" :project="$project"/>
             </div>
         </div>
