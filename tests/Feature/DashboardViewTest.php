526e2a37dc197ae0499f03490930e1673b930f82
KaungZayY <kaungzayyan19@gmail.com>
2024-11-16 19:37:00 +0700

    Dashboard Feature Test : note - Sail setup auto creates separate test db for testing, need to configure if not running on sail

---
+++ tests/Feature/DashboardViewTest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Livewire\Dashboard;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class DashboardViewTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_redirect_to_login_page_if_not_authenticated(): void
+    {
+        $response = $this->get('/');
+
+        $response->assertRedirect('/login');
+        $response->assertSee('Register');
+        $response->assertStatus(302);
+    }
+
+    public function test_dashboard_component_exists_on_dashboard_view_when_user_logged_in(): void
+    {
+        $user = User::factory()->create();
+
+        $response = $this->actingAs($user)->get('/');
+
+        $response->assertSeeLivewire(Dashboard::class);
+        $response->assertStatus(200);
+    }
+}
