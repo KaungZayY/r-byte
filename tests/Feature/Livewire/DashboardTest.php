e91eb56a1071ea665efd9845bd91568fd728c2cf
KaungZayY <kaungzayyan19@gmail.com>
2024-11-23 04:12:35 +0700

    Feature Test: Dashboard Livewire Functions

--- tests/Feature/Livewire/DashboardTest.php
+++ tests/Feature/Livewire/DashboardTest.php
@@ -4,9 +4,10 @@ namespace Tests\Feature\Livewire;
 
 use App\Livewire\Dashboard;
 use App\Models\Project;
+use App\Models\Team;
+use App\Models\Teammate;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Livewire\Livewire;
 use Tests\TestCase;
 
@@ -28,7 +29,7 @@ class DashboardTest extends TestCase
     }
 
     /** @test */
-    public function check_projects_are_displayed_if_exists(): void
+    public function check_own_projects_are_displayed_if_exists(): void
     {
         $user = User::factory()->create();
 
@@ -38,6 +39,70 @@ class DashboardTest extends TestCase
             ->test(Dashboard::class)
             ->assertViewHas('projects', function ($collection) use ($projects){
                 return $projects->every(fn($project) => $collection->contains('id', $project->id));
+            });
+    }
+
+    /** @test */
+    public function check_team_projects_are_displayed_if_exists(): void
+    {
+        $authUser = User::factory()->create();
+
+        $projectOwner = User::factory()->create();
+        $project = Project::factory()->create(['created_by' => $projectOwner->id]);
+        $team = Team::factory()->create(['project_id' => $project->id]);
+        Teammate::factory()->create(['user_id' => $authUser->id, 'team_id' => $team->id]);
+
+        Livewire::actingAs($authUser)
+            ->test(Dashboard::class)
+            ->assertViewHas('projects', function ($collection) use ($project){
+                return $collection->contains($project);
+            });
+    }
+
+    /** @test */
+    public function check_if_sorting_asc_works(): void
+    {
+        $user = User::factory()->create();
+
+        Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Project A']);
+        Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Project Z']);
+
+        Livewire::actingAs($user)
+            ->test(Dashboard::class)
+            ->set('sort', 'ASC')
+            ->assertSeeInOrder(['Project A', 'Project Z']);
+    }
+
+    /** @test */
+    public function check_if_sorting_desc_works(): void
+    {
+        $user = User::factory()->create();
+
+        Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Project A']);
+        Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Project Z']);
+
+        Livewire::actingAs($user)
+            ->test(Dashboard::class)
+            ->set('sort', 'DESC')
+            ->assertSeeInOrder(['Project Z', 'Project A']);
+    }
+
+    /** @test */
+    public function check_if_search_works(): void
+    {
+        $user = User::factory()->create();
+
+        $matchingProject = Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Matching Project']);
+        $unrelatedProject = Project::factory()->create(['created_by' => $user->id, 'project_name' => 'Unrelated Project']);
+
+        Livewire::actingAs($user)
+            ->test(Dashboard::class)
+            ->set('search', 'Matching')
+            ->assertViewHas('projects', function ($collection) use ($matchingProject){
+                return $collection->contains($matchingProject);
+            })
+            ->assertViewHas('projects', function ($collection) use ($unrelatedProject) {
+                return !$collection->contains($unrelatedProject);
             });
     }
 }
